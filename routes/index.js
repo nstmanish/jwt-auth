ccd83d323c4841fd1ae015be3a76b7ffaa1ac1e3
nstmanish <manish.chourasiya@neosoftmail.com>
2022-05-31 10:46:08 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const auth = require("./middleware/auth");
-
+const auth = require('../middleware/auth');
 
 router.get('/', auth, function(req, res, next) {
   res.render('index', { title: 'Express' });
